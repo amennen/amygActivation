e993c2eb441716d5bebdc6fa775fef9b42ae9cbc
AnneM <amennen@princeton.edu>
2020-05-22 16:39:25 -0400

    finalize script

--- finalize.py
+++ finalize.py
@@ -30,84 +30,95 @@ import rtCommon.projectUtils as projUtils
 from rtCommon.structDict import StructDict
 from rtCommon.dicomNiftiHandler import getTransform
 from projects.amygActivation.initialize import initialize
+from projects.amygActivation.amygActivation import getRunFilename
 
 # obtain the full path for the configuration toml file
 defaultConfig = os.path.join(currPath, 'conf/amygActivation.toml')
 
 def finalize(cfg, args):
-
-	return
+	# first find the number of runs completed
+	run_path = cfg.local.subject_full_day_path + '/' + 'run*'
+	if args.filesremote:
+		run_path = cfg.server.subject_full_day_path + '/' + 'run*'
+	nRuns_completed = len(glob.glob(run_path))
+	return nRuns_completed
 
 def main(argv=None):
-    """
-    This is the main function that is called when you run 'finialize.py'.
-    
-    Here, you will load the configuration settings specified in the toml configuration 
-    file, initiate the class fileInterface, and set up some directories and other 
-    important things through 'finalize()'
-    """
+	"""
+	This is the main function that is called when you run 'finialize.py'.
 
-    # define the parameters that will be recognized later on to set up fileIterface
-    argParser = argparse.ArgumentParser()
-    argParser.add_argument('--config', '-c', default=defaultConfig, type=str,
-                           help='experiment config file (.json or .toml)')
-    # This parameter is used for projectInterface
-    argParser.add_argument('--commpipe', '-q', default=None, type=str,
-                           help='Named pipe to communicate with projectInterface')
-    argParser.add_argument('--filesremote', '-x', default=False, action='store_true',
-                           help='retrieve files from the remote server')
-    argParser.add_argument('--addr', '-a', default='localhost', type=str, 
-               help='server ip address')
-    argParser.add_argument('--runs', '-r', default='', type=str,
-                       help='Comma separated list of run numbers')
-    argParser.add_argument('--scans', '-s', default='', type=str,
-                       help='Comma separated list of scan number')
-    args = argParser.parse_args(argv)
+	Here, you will load the configuration settings specified in the toml configuration 
+	file, initiate the class fileInterface, and set up some directories and other 
+	important things through 'finalize()'
+	"""
 
-    # load the experiment configuration file
-    cfg = utils.loadConfigFile(args.config)
-    cfg = initialize(cfg, args)
+	# define the parameters that will be recognized later on to set up fileIterface
+	argParser = argparse.ArgumentParser()
+	argParser.add_argument('--config', '-c', default=defaultConfig, type=str,
+						   help='experiment config file (.json or .toml)')
+	# This parameter is used for projectInterface
+	argParser.add_argument('--commpipe', '-q', default=None, type=str,
+						   help='Named pipe to communicate with projectInterface')
+	argParser.add_argument('--filesremote', '-x', default=False, action='store_true',
+						   help='retrieve files from the remote server')
+	argParser.add_argument('--addr', '-a', default='localhost', type=str, 
+			   help='server ip address')
+	argParser.add_argument('--runs', '-r', default='', type=str,
+					   help='Comma separated list of run numbers')
+	argParser.add_argument('--scans', '-s', default='', type=str,
+					   help='Comma separated list of scan number')
+	args = argParser.parse_args(argv)
 
-    # copy subject folders from server to local
-    # subject-specific folder
-    # everything in temp/convertedNiftis
-    if args.filesremote:
+	# load the experiment configuration file
+	cfg = utils.loadConfigFile(args.config)
+	cfg = initialize(cfg, args)
 
-        # open up the communication pipe using 'projectInterface'
-        projectComm = projUtils.initProjectComm(args.commpipe, args.filesremote)
+	nRunsCompleted = finalize(cfg,args)
+	# copy subject folders from server to local
+	# subject-specific folder
+	# everything in temp/convertedNiftis
+	if args.filesremote:
 
-        # initiate the 'fileInterface' class, which will allow you to read and write 
-        #   files and many other things using functions found in 'fileClient.py'
-        #   INPUT:
-        #       [1] args.filesremote (to retrieve dicom files from the remote server)
-        #       [2] projectComm (communication pipe that is set up above)
-        fileInterface = FileInterface(filesremote=args.filesremote, commPipes=projectComm)
+		# open up the communication pipe using 'projectInterface'
+		projectComm = projUtils.initProjectComm(args.commpipe, args.filesremote)
 
-        # we don't need the tmp/convertedNiftis so first remove those
-        tempNiftiDir = os.path.join(cfg.server.dataDir, 'tmp/convertedNiftis/')
-        projUtils.deleteFolder(tempNiftiDir)
+		# initiate the 'fileInterface' class, which will allow you to read and write 
+		#   files and many other things using functions found in 'fileClient.py'
+		#   INPUT:
+		#       [1] args.filesremote (to retrieve dicom files from the remote server)
+		#       [2] projectComm (communication pipe that is set up above)
+		fileInterface = FileInterface(filesremote=args.filesremote, commPipes=projectComm)
 
-        # next, get the final patterns data
-        # maybe have them inuput for each run as an argument? specify all the run numbers or it'll look for all 
-        # run folders
-                run_filename = getRunFilename(cfg.sessionId, run)
-        full_run_filename_to_save = os.path.join(runFolder, run_filename)
-        srcPattern = cfg.
-        downloadFilesFromCloud(fileInterface, srcFilePattern, outputDir, deleteAfter=False)
+		# we don't need the tmp/convertedNiftis so first remove those
+		tempNiftiDir = os.path.join(cfg.server.dataDir, 'tmp/convertedNiftis/')
+		projUtils.deleteFolder(tempNiftiDir)
 
+		# next, go through each run and put each run data into local run folder
+		for r in np.arange(nRunsCompleted):
+			runNum = r + 1 # run numbers start at 1
+			runId = 'run-{0:02d}'.format(runNum)
+			runFolder = cfg.server.subject_full_day_path + '/' + runId + '/*'
+			listOfFiles = glob.glob(runFolder)
+			runFolder_local = cfg.local.subject_full_day_path + '/' + runId 
+			projUtils.downloadFilesFromList(fileInterface, listOfFiles, runFolder_local)
 
+		# next delete the entire subject folder on the cloud
+		# MAKE SURE THIS IS CORRECT FOR YOUR EXPERIMENT BEFORE YOU RUN
+		subject_dir = '{0}/{1}'.format(cfg.server.dataDir, cfg.bids_id)
+		print('FOLDER TO DELETE ON CLOUD SERVER: ', subject_dir)
+		if cfg.server.deleteAfter:
+			print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+			print('DELETING SUBJECT FOLDER ON CLOUD SERVER: ', subject_dir)
+			print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+			projUtils.deleteFolder(subject_dir)
 
-        #projUtils.uploadFolderToCloud(fileInterface,cfg.local.wf_dir,cfg.server.wf_dir)
-
-        # upload ROI folder to cloud server
-        #projUtils.uploadFolderToCloud(fileInterface,cfg.local.maskDir,cfg.server.maskDir)
-    return 0
+	return 0
 
 if __name__ == "__main__":
-    """
-    If 'finalize.py' is invoked as a program, then actually go through all of the 
-    portions of this script. This statement is not satisfied if functions are called 
-    from another script using "from finalize.py import FUNCTION"
-    """    
-    main()
-    sys.exit(0)
+	"""
+	If 'finalize.py' is invoked as a program, then actually go through all of the 
+	portions of this script. This statement is not satisfied if functions are called 
+	from another script using "from finalize.py import FUNCTION"
+	"""    
+	main()
+	sys.exit(0)
